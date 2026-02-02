c0ec2118b388adc208b16f9135db6e7368a514d0
zacken <mchereau05@gmail.com>
2026-02-02 17:23:30 +0100

    app.js and index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import CategoryPage from './pages/CategoryPage';
+import ArtisanDetailPage from './pages/ArtisanDetailPage';
+import LegalPage from './pages/LegalPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <Header />
+        
+        <main className="flex-grow-1">
+          <Routes>
+            {/* Page d'accueil */}
+            <Route path="/" element={<HomePage />} />
+            
+            {/* Page catégorie */}
+            <Route path="/categorie/:id" element={<CategoryPage />} />
+            
+            {/* Page détails artisan */}
+            <Route path="/artisan/:id" element={<ArtisanDetailPage />} />
+            
+            {/* Pages légales */}
+            <Route 
+              path="/mentions-legales" 
+              element={<LegalPage title="Mentions légales" />} 
+            />
+            <Route 
+              path="/donnees-personnelles" 
+              element={<LegalPage title="Données personnelles" />} 
+            />
+            <Route 
+              path="/accessibilite" 
+              element={<LegalPage title="Accessibilité" />} 
+            />
+            <Route 
+              path="/cookies" 
+              element={<LegalPage title="Cookies" />} 
+            />
+            
+            {/* Page 404 - doit être en dernier */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
